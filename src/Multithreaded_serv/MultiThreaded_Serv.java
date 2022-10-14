9040ffe02ac964c13a2ff076a2323b209aa79b81
PtravisCS <21698432+PtravisCS@users.noreply.github.com>
2022-10-14 14:20:16 -0400

    Moved try catch outside of while loop

--- src/Multithreaded_serv/MultiThreaded_Serv.java
+++ src/Multithreaded_serv/MultiThreaded_Serv.java
@@ -11,21 +11,22 @@ public class MultiThreaded_Serv {
 
   public static void main(String[] args) {
     Boolean run = true; //Not really different than while(true) but it allows for a mutable value so that the program theoretically could be set up to exit in the future if desired.
-    while(run) {
-      try (ServerSocket servSock = new ServerSocket(8080);){ //This feels messy as the socket is thrown away and re-created every time the program loops, but I couldn't find a better way to do it.
-        Socket socket = servSock.accept(); 
-        ProcessReq procReq = new ProcessReq(socket); //Create main server class and feed it the previously created socket.
-        Thread t = new Thread(procReq); //Create new thread using the main server class
-        t.start(); //start the thread
-      } catch (IOException e) {
-        System.out.println("IOException Occurred, ");
-        System.out.println(e);
-      } catch (Exception e) {
-        System.out.println("That Was Unexpected");
-        System.out.println("Exception Ocurred :" );
-        System.out.println(e);
-        run = false;
+    
+    try (ServerSocket servSock = new ServerSocket(8080);){ //This feels messy as the socket is thrown away and re-created every time the program loops, but I couldn't find a better way to do it.
+      while(run) {
+          Socket socket = servSock.accept(); 
+          ProcessReq procReq = new ProcessReq(socket); //Create main server class and feed it the previously created socket.
+          Thread t = new Thread(procReq); //Create new thread using the main server class
+          t.start(); //start the thread
       }
+    } catch (IOException e) {
+      System.out.println("IOException Occurred, ");
+      System.out.println(e);
+    } catch (Exception e) {
+      System.out.println("That Was Unexpected");
+      System.out.println("Exception Ocurred :" );
+      System.out.println(e);
+      run = false;
     }
   }
 
